cfe2984ed98dcf78fd7a122b0e475d5527db988e
Jess Daniel <jessdaniel474@gmail.com>
2025-06-17 22:23:33 -0400

    made static cause vercel package broke

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
-import vercelServerless from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.michaeljdaniel.com',
-  integrations: [mdx(), sitemap(), tailwind()],
-  output: 'server',
-
-  adapter: vercelServerless()
+  integrations: [mdx(), tailwind()],
+  output: 'static'
 });
